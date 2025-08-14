bbebcd9f266f35b0d8f94d617f65fd61d23baf5c
filipefogaca <filipe160805@gmail.com>
2025-08-14 09:55:14 -0300

    Update pratica.py
    atualização do código

--- aulapratica/pratica.py
+++ aulapratica/pratica.py
@@ -1,3 +1,8 @@
 print("olá filipe, seja bem vindo !")
-status = input("você esta bem?")
-
+status = input("você esta bem? (s/n): ")
+if status == "s":
+  print("que bon, fico feliz!")
+elif status == "n":
+  print("sinto muito, melhoras.")
+else:
+  print("tente novamente com (s/n)")
