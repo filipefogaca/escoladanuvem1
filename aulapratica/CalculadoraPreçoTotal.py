a3cbb75a121559caf373996ce760b9eaf4807d1e
Filipe <filipe160805@gmail.com>
2025-08-18 09:46:37 -0300

    atualização

--- aulapratica/CalculadoraPreçoTotal.py
+++ aulapratica/CalculadoraPreçoTotal.py
@@ -2,4 +2,4 @@ produto = "Cadeira Infantil"
 preco = float(12.40)
 quantidade = 3
 precoTotal= preco * quantidade
-print(f" {quantidade} {produto}, preço total: {precoTotal}")
+print(f" {quantidade} {produto}, preço total: {precoTotal:.2f}")
