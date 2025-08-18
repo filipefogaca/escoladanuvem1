d7dcb9ae56738e53114b1d56e8e6a99758eb1ae2
filipefogaca <filipe160805@gmail.com>
2025-08-18 09:55:02 -0300

    Create CalculadoraNúmeroInt.py

---
+++ aulapratica/CalculadoraNúmeroInt.py
@@ -0,0 +1,7 @@
+A = int(input("digite o 1° numero: ")
+B = int(input("digite o 2º numero: ")
+C = int(input("digite o 3º numero: ")
+D = int(input("digite o 4º numero: ")
+
+diferenca = ((A * B) - (C * D))
+print(f"DIFERENCA = {diferenca}")
