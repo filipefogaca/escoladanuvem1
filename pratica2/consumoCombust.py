5c67cfa57225f4c6a7e4902058c80a96d317d864
Filipe <filipe160805@gmail.com>
2025-08-19 11:28:39 -0300

    atualização

---
+++ pratica2/consumoCombust.py
@@ -0,0 +1,6 @@
+distanciaP = 300
+combustivelG = 25
+
+consumoMedio = distanciaP / combustivelG
+print(f"Distância percorrida > {distanciaP} KM\nCombustível gasto > {combustivelG} Litros")
+print(f"Consumo médio > {consumoMedio:.2f} KM/L")
