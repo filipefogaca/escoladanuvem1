2cd7c59b6757db48c083b5202ad9a08a1662ef85
Filipe <filipe160805@gmail.com>
2025-08-19 11:11:00 -0300

    atualização

---
+++ pratica2/conversorMoeda.py
@@ -0,0 +1,7 @@
+valorR = 100.00
+taxaD = 5.60
+taxaE = 6.60
+reais_para_dolar = valorR / taxaD
+reais_para_euro = valorR / taxaE
+print(f"converção de R${valorR:.2f} para Dólar: {reais_para_dolar:.2f}")
+print(f"converção de R${valorR:.2f} para Euro: {reais_para_euro:.2f}")
